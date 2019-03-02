daad247b7a3a1c46f5b07019e8b193584420828e
Tobi Ola <oluwatobi.ola@gmail.com>
2019-03-02 11:44:36 -0600

    added data upload to firebase function

--- iOSApplication/BoilerFlex/CameraViewController.swift
+++ iOSApplication/BoilerFlex/CameraViewController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 import CoreML
+import Alamofire
+import SwiftyJSON
+import FirebaseFirestore
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -69,6 +72,130 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         
         photoView.image = image
         percentage.text = "\(result.classLabel) - \(converted) %"
+        
+        let headers: HTTPHeaders = [
+            "x-app-id": "a51d3f5d",
+            "x-app-key": "0cc6ebdbce2242015fc21ec1de96c780"
+        ]
+        
+        var urlString = "https://trackapi.nutritionix.com/v2/search/instant?query=" + result.classLabel
+        urlString = urlString.replacingOccurrences(of: "_", with: "%20")
+
+        let url = URL(string: urlString)!
+        
+        Alamofire.request(url, headers: headers).responseJSON { (response) in
+            
+            guard response.result.error == nil else {
+                // got an error in getting the data, need to handle it
+                print("error calling GET")
+                print(response.result.error!)
+                return
+            }
+            
+            if let value = response.result.value {
+                let nixItemId = JSON(value)["branded"].array?[0]["nix_item_id"].string
+                let urlString = "https://trackapi.nutritionix.com/v2/search/item?nix_item_id=" + nixItemId!
+                print(urlString)
+                let url = URL(string: urlString)!
+                Alamofire.request(url, headers: headers).responseJSON(completionHandler: { (res) in
+                    guard res.result.error == nil else {
+                        // got an error in getting the data, need to handle it
+                        print("error calling GET")
+                        print(res.result.error!)
+                        return
+                    }
+                    
+                    if let value = res.result.value {
+                        
+                        let json = JSON(value) // JSON comes from SwiftyJSON
+                        print("printing nix item")
+                        print(json) // to see the JSON response
+                        let foodData = json["foods"].array?[0]
+                        let db = Firestore.firestore()
+                        
+                        let foodName = foodData?["food_name"].string
+                        let calories = foodData?["nf_calories"].number
+                        let cholesterol = foodData?["nf_cholesterol"].number
+                        let carbs = foodData?["nf_total_carbohydrate"].number
+                        let sugars = foodData?["nf_sugars"].number
+                        let photoUrl = foodData?["photo"]["thumb"].string
+                        let sodium = foodData?["nf_sodium"].number
+                        let protein = foodData?["nf_protein"].number
+                        let fat = foodData?["nf_total_fat"].number
+                        let saturatedFat = foodData?["nf_saturated_fat"].number
+                        
+                        db.collection("logs").addDocument(data: [
+                            "food_name": foodName!,
+                            "calories": calories!,
+                            "cholesterol": cholesterol!,
+                            "carbs": carbs!,
+                            "sugars": sugars!,
+                            "photo_url": photoUrl!,
+                            "sodium": sodium!,
+                            "protein": protein!,
+                            "fat": fat!,
+                            "saturated_fat": saturatedFat!,
+                            "time": Timestamp(date: Date())
+                        ])
+                    }
+                })
+            }
+            
+            
+        }
+        /*
+            
+            if let value = response.result.value {
+                
+                let json = JSON(value) // JSON comes from SwiftyJSON
+                let foodData = json["branded"].array?[0]
+                let db = Firestore.firestore()
+                
+                let foodName = foodData?["food_name"].string
+                let nixItemId = JSON(value)["branded"].array?[0]["nix_item_id"].string
+                
+                let urlString = "https://trackapi.nutritionix.com/v2/search/item?nix_item_id=" + nixItemId!
+                
+                let url = URL(string: urlString)!
+
+                Alamofire.request(url, headers: headers).responseJSON(completionHandler: { (response) in
+                    
+                    guard response.result.error == nil else {
+                        // got an error in getting the data, need to handle it
+                        print("error calling GET")
+                        print(response.result.error!)
+                        return
+                    }
+                    
+                    if let value = response.result.value {
+                        
+                        let json = JSON(value) // JSON comes from SwiftyJSON
+                        print(json) // to see the JSON response
+                        let foodData = json["branded"].array?[0]
+                        let db = Firestore.firestore()
+                        
+                        let foodName = foodData?["food_name"].string
+                    
+                    }
+                }
+                
+                    /*
+                db.collection("logs").addDocument(data: [
+                    "food_name": foodName ?? "food unknown", //foodData["foot_name"] as? String,
+                    "calories": "420"
+                ]) { err in
+                    if let err = err {
+                        print("Error adding document: \(err)")
+                    } else {
+                        print("Document added")
+                    }
+                }*/
+                
+                }
+            }
+        }
+        */
+        
     }
     
     override func didReceiveMemoryWarning() {
