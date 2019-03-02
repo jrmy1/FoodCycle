75525f12ca8ce25896415b0a40e65eb1cd9f091d
moldingtofu <jeremykchang@gmail.com>
2019-03-02 02:31:23 -0500

    initial app

---
+++ BoilerFlex/CameraViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CameraViewController.swift
+//  BoilerFlex
+//
+//  Created by jeremy on 3/1/19.
+//  Copyright © 2019 Jeremy Chang. All rights reserved.
+//
+
+import UIKit
+import CoreML
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    let model = Food101()
+
+}
+
