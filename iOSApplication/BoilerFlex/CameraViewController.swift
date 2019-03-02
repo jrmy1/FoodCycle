753ca6c0dbcfa6e2b147a0b4b0ccf69bf6367aa7
moldingtofu <jeremykchang@gmail.com>
2019-03-02 04:16:39 -0500

    why

--- iOSApplication/BoilerFlex/CameraViewController.swift
+++ iOSApplication/BoilerFlex/CameraViewController.swift
@@ -2,21 +2,75 @@
 //  CameraViewController.swift
 //  BoilerFlex
 //
-//  Created by jeremy on 3/1/19.
+//  Created by jeremy on 3/2/19.
 //  Copyright © 2019 Jeremy Chang. All rights reserved.
 //
 
 import UIKit
 import CoreML
 
-class ViewController: UIViewController {
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet var photoView: UIImageView!
+    @IBOutlet var percentage: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    let model = Food101()
-
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let imagePickerView = UIImagePickerController()
+        imagePickerView.delegate = self
+        
+        alert.addAction(UIAlertAction(title: "Choose Image", style: .default) { _ in
+            imagePickerView.sourceType = .photoLibrary
+            self.present(imagePickerView, animated: true, completion: nil)
+        })
+        
+        alert.addAction(UIAlertAction(title: "Take Image", style: .default) { _ in
+            imagePickerView.sourceType = .camera
+            self.present(imagePickerView, animated: true, completion: nil)
+        })
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
+        dismiss(animated: true, completion: nil)
+        
+        guard let image = info["UIImagePickerControllerOriginalImage"] as? UIImage else {
+            return
+        }
+        
+        processImage(image)
+    }
+    
+    func processImage(_ image: UIImage) {
+        let model = Food101()
+        let size = CGSize(width: 299, height: 299)
+        
+        guard let buffer = image.resize(to: size)?.pixelBuffer() else {
+            fatalError("Scaling or converting to pixel buffer failed!")
+        }
+        
+        guard let result = try? model.prediction(image: buffer) else {
+            fatalError("Prediction failed!")
+        }
+        
+        let confidence = result.foodConfidence["\(result.classLabel)"]! * 100.0
+        let converted = String(format: "%.2f", confidence)
+        
+        imageView.image = image
+        percentage.text = "\(result.classLabel) - \(converted) %"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
-
