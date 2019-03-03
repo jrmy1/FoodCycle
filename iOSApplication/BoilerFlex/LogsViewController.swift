fb64ca15add8c38042869f116d3b7ba9461ec394
moldingtofu <jeremykchang@gmail.com>
2019-03-02 21:00:40 -0500

    logs

--- iOSApplication/BoilerFlex/LogsViewController.swift
+++ iOSApplication/BoilerFlex/LogsViewController.swift
@@ -11,44 +11,74 @@ import FirebaseFirestore
 
 class LogsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var logs = [QueryDocumentSnapshot]()
-
-    @IBOutlet weak var logTable: UITableView!
+    var logs: [QueryDocumentSnapshot] = []
+    @IBOutlet weak var tableView: UITableView!
+    let refreshLogs = UIRefreshControl()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+
         refresh()
-        self.logTable.reloadData()
-
-
+        refreshLogs.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        tableView.refreshControl = refreshLogs
     }
     
-    func refresh() {
+    @objc func refresh() {
+        let settings = FirestoreSettings()
+        Firestore.firestore().settings = settings
         let db = Firestore.firestore()
         db.collection("logs").getDocuments { (snapshot, error) in
-            self.logs = (snapshot?.documents)!
-            print(self.logs[0].data())
-            
-            self.logTable.reloadData()
+            if let error = error {
+                print(error)
+            }
+            else {
+                self.logs = snapshot!.documents
+                print(self.logs[0].data())
+                self.tableView.reloadData()
+            }
+        }
+        self.refreshLogs.endRefreshing()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let db = Firestore.firestore()
+        db.collection("logs").getDocuments { (snapshot, error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                self.logs = snapshot!.documents
+                self.tableView.reloadData()
+            }
         }
     }
     
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print(self.logs.count)
         return self.logs.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "LogCell") as! LogCell
-        
         let log = self.logs[indexPath.row].data()
-        print(self.logs[indexPath.row].data())
-        cell.calorieCountLabel.text = log["calories"] as? String
+        
+        cell.calorieCountLabel.text = String(log["calories"] as! Int)
         cell.foodNameLabel.text = log["food_name"] as? String
+        
+        let rawdate = (log["time"] as! Timestamp).dateValue()
+        print(rawdate)
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let date = formatter.string(from: rawdate)
+        
+        cell.dateLabel.text = date
         
         return cell
     }
